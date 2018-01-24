019f4f31d3bfb7f6737d0804e6f5c3158e7493c1
Sean Derrow <sderrow@mit.edu>
2018-01-24 04:13:16 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,3 +39,11 @@ def winner(board)
     board[winning[0]]
   end
 end
+
+def turn_count(board)
+  (board.collect { |x| x == " " ? 0 : 1 }).inject(:+)
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
